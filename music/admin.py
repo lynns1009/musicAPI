f491831fc6fcd19081a163fa87a5a0518a6e5006
Lynns <Lynns1009@users.noreply.github.com>
2018-11-20 16:39:56 +1000

    Add music and album models

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album,Music
+
+
+@admin.register(Music)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ['title','lyrics','alb_re']
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['name','author','publish_year','style']
--- music/models.py
+++ music/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+STYLE=[('J','Jazz'),('P','Pop music'),('F','Folk music'),('B','Blue'),('C','Country music'),('M','Mental')]
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=100,blank=True,default='')
+    author = models.CharField(max_length=100, blank=True, default='')
+    publish_year = models.DateTimeField()
+    style=models.CharField(choices=STYLE,max_length=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Music(models.Model):
+    title = models.CharField(max_length=100,blank=True,default='')
+    lyrics = models.TextField()
+    alb_re=models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
